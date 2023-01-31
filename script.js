c107f49cdaa012489fdb694537f2ec1686cc0d87
earljoves <jovesearl@gmail.com>
2023-01-30 21:42:03 -0600

    Lay out basic unformatted framework

---
+++ script.js
@@ -0,0 +1,54 @@
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+
+// Guess 1
+const g1l1 = document.getElementById('g1l1');
+const g1l2 = document.getElementById('g1l2');
+const g1l3 = document.getElementById('g1l3');
+const g1l4 = document.getElementById('g1l4');
+const g1l5 = document.getElementById('g1l5');
+const guess1 = [g1l1, g1l2, g1l3, g1l4, g1l5];
+
+// Guess 2
+const g2l1 = document.getElementById('g2l1');
+const g2l2 = document.getElementById('g2l2');
+const g2l3 = document.getElementById('g2l3');
+const g2l4 = document.getElementById('g2l4');
+const g2l5 = document.getElementById('g2l5');
+const guess2 = [g2l1, g2l2, g2l3, g2l4, g2l5];
+
+// Guess 3
+const g3l1 = document.getElementById('g3l1');
+const g3l2 = document.getElementById('g3l2');
+const g3l3 = document.getElementById('g3l3');
+const g3l4 = document.getElementById('g3l4');
+const g3l5 = document.getElementById('g3l5');
+const guess3 = [g3l1, g3l2, g3l3, g3l4, g3l5];
+
+// Guess 4
+const g4l1 = document.getElementById('g4l1');
+const g4l2 = document.getElementById('g4l2');
+const g4l3 = document.getElementById('g4l3');
+const g4l4 = document.getElementById('g4l4');
+const g4l5 = document.getElementById('g4l5');
+const guess4 = [g4l1, g4l2, g4l3, g4l4, g4l5];
+
+// Guess 5
+const g5l1 = document.getElementById('g5l1');
+const g5l2 = document.getElementById('g5l2');
+const g5l3 = document.getElementById('g5l3');
+const g5l4 = document.getElementById('g5l4');
+const g5l5 = document.getElementById('g5l5');
+const guess5 = [g5l1, g5l2, g5l3, g5l4, g5l5];
+
+// Guess 6
+const g6l1 = document.getElementById('g6l1');
+const g6l2 = document.getElementById('g6l2');
+const g6l3 = document.getElementById('g6l3');
+const g6l4 = document.getElementById('g6l4');
+const g6l5 = document.getElementById('g6l5');
+const guess6 = [g6l1, g6l2, g6l3, g6l4, g6l5];
+
+let currentGuess = 1;
+addEventListener('keydown', (event) => {
+    TODO
+});
